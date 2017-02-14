7fff070e876963e835561f09e758f0e3a49b9870
nsbradford <nsbradford@gmail.com>
2017-02-13 22:01:56 -0500

    Use background segmentation to eliminate propeller motion.

--- lanes/lanes.py
+++ lanes/lanes.py
@@ -3,13 +3,20 @@
     Nicholas S. Bradford
     12 Feb 2017
 
+    Notes:
+        Color doesn't really seem to work; inaccurate on areas not directly in front of plane 
+
 """
 
 import cv2 # for reading photos and videos
 import numpy as np
-from showmany import multi_plot
 
 
+def addPerspectivePoints(img, topLeft, topRight, bottomLeft, bottomRight):
+    cv2.circle(img, topLeft, radius=5, color=(0,0,255))
+    cv2.circle(img, topRight, radius=5, color=(0,0,255))
+    cv2.circle(img, bottomLeft, radius=5, color=(0,0,255))
+    cv2.circle(img, bottomRight, radius=5, color=(0,0,255))
 
 def getPerspectiveMatrix(topLeft, topRight, bottomLeft, bottomRight):
     # pts1 = np.float32([[382, 48], [411, 48], [292, 565], [565, 565]])
@@ -30,21 +37,74 @@ def extractColor(img):
 
     # define range of blue color in HSV
     # yellow: cvScalar(20, 100, 100), cvScalar(30, 255, 255)
-    # lower_blue = np.array([110,50,50])
-    # upper_blue = np.array([130,255,255])
-    lower_yellow = np.array([10, 100, 50])
-    upper_yellow = np.array([40, 255, 255])
+    lower_yellow = np.array([10, 70, 30])
+    upper_yellow = np.array([60, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(img, img, mask= mask)
 
-    answer = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
-    return answer
+    # answer = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+
+    kernel = np.ones((5,5), np.uint8)
+    dilated = cv2.dilate(res, kernel, iterations=5)
+    morphed = cv2.erode(dilated, kernel, iterations=5)
+    return morphed
 
 
-def video_demo(highres_scale=0.5):
+def extractEdges(img):
+    edges = cv2.Canny(img, 20, 100, apertureSize=3)
+    bgrEdges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    return bgrEdges
+
+
+def addLines(img):
+    copy = img.copy()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    lines = cv2.HoughLines(image=gray, rho=1, theta=np.pi/180, threshold=100)
+    # print lines
+    # print len(lines)
+    # print len(lines[0])
+    if lines is not None:
+        for line in lines: #[0]:
+            #print line
+            rho,theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)        
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            cv2.line(img=copy, pt1=(x1,y1), pt2=(x2,y2), color=(255,0,0), thickness=2)
+    return copy
+
+def dilateAndErode(original):
+    # kernel = np.ones((10,10),np.uint8)
+    # closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    # return closed
+    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    size = np.size(gray)
+    skel = np.zeros(gray.shape,np.uint8)
+    ret,img = cv2.threshold(gray,20,255,0)
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+    done = False
+    while not done:
+        eroded = cv2.erode(img,element)
+        temp = cv2.dilate(eroded,element)
+        temp = cv2.subtract(img,temp)
+        skel = cv2.bitwise_or(skel,temp)
+        img = eroded.copy()
+        zeros = size - cv2.countNonZero(img)
+        if zeros==size:
+            done = True
+    colorSkel = cv2.cvtColor(skel, cv2.COLOR_GRAY2BGR)
+    return colorSkel
+
+
+def getPerspectivePoints(highres_scale):
     original_width = 1920
     original_height = 1080
     scaled_width = int(highres_scale * original_width)
@@ -53,43 +113,52 @@ def video_demo(highres_scale=0.5):
     wing_height = int(scaled_height * 2.0 / 3.0)
     right_width = int(scaled_width * 2.0 / 3.0)
     left_width = int(scaled_width / 3.0)
-
     topLeft = (int(scaled_width / 2.0 - 50), horizon_height + 50)
     topRight = (int(scaled_width / 2.0 + 50), horizon_height + 50)
     bottomLeft = (left_width, wing_height)
     bottomRight = (right_width, wing_height)
+    return topLeft, topRight, bottomLeft, bottomRight
 
-    perspectiveMatrix = getPerspectiveMatrix(topLeft, topRight, bottomLeft, bottomRight)
 
+def resizeFrame(img, scale):
+    return cv2.resize(img, dsize=None, fx=scale, fy=scale)
+
+
+def openVideo():
     print('Load video...')
     prefix = '../../'
     cap = cv2.VideoCapture(prefix + 'taxi_trim.mp4') # framerate of 29.97
+    # print('Frame size:', frame.shape) # 1920 x 1080 original, 960 x 540 resized
+    return cap
+
+
+def video_demo(highres_scale=0.5, scaled_height=540):
+    topLeft, topRight, bottomLeft, bottomRight = getPerspectivePoints(highres_scale)
+    perspectiveMatrix = getPerspectiveMatrix(topLeft, topRight, bottomLeft, bottomRight)
+    fgbg = cv2.createBackgroundSubtractorMOG2()
+    cap = openVideo()
     while(cap.isOpened()):
         ret, frame = cap.read()
-        if not ret:
-            break;
-        img = cv2.resize(frame, dsize=None, fx=1.0 * highres_scale, fy=1.0 * highres_scale)
-        # print('Frame size:', frame.shape) # 1920 x 1080 original, 960 x 540 resized
+        img = resizeFrame(frame, highres_scale)
         perspective = cv2.warpPerspective(img, perspectiveMatrix, (scaled_height,scaled_height) )
-        colored = extractColor(perspective)
+        
+        fgmask = fgbg.apply(perspective, learningRate=0.5)
+        # mask = cv2.bitwise_not(fgmask)
+        # background = cv2.bitwise_and(perspective, perspective, mask=mask)
+        background = fgbg.getBackgroundImage()
 
-        cv2.circle(img, topLeft, radius=5, color=(0,0,255))
-        cv2.circle(img, topRight, radius=5, color=(0,0,255))
-        cv2.circle(img, bottomLeft, radius=5, color=(0,0,255))
-        cv2.circle(img, bottomRight, radius=5, color=(0,0,255))
+        # edges = extractEdges(perspective)
+        colored = extractColor(background)
+        morphed = dilateAndErode(colored)
+        lines = addLines(morphed)
+        addPerspectivePoints(img, topLeft, topRight, bottomLeft, bottomRight)
+        cv2.imshow('combined', np.hstack((perspective, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR), colored, lines)))
 
-        # cv2.imshow('resized', img)
-        # cv2.imshow('perspective', perspective)
-        # multi_plot("Images", 2, img, perspective);
-        combined = np.hstack((img, perspective, colored))
-        cv2.imshow('combined', combined)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'): # 1000 / 29.97 = 33.37
+        if cv2.waitKey(1) & 0xFF == ord('q'): # 1000 / 29.97 = 33.37
             break
     cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == '__main__':
-    # time_score()2
-    # main()
     video_demo()
