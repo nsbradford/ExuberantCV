6d9d3c55b537b05af0f7079039b524a79768bb4a
nsbradford <nsbradford@gmail.com>
2017-03-21 13:33:44 -0400

    Refactor; may need to reduce Saturation of HSV-yellow.

--- lanes/runner.py
+++ lanes/runner.py
@@ -1,3 +1,7 @@
+"""
+    runner.py
+
+"""
 
 import cv2
 
@@ -17,15 +21,14 @@ def resizeFrame(img, scale):
 
 
 def pictureDemo(path, highres_scale=0.5, scaled_height=Constants.IMG_SCALED_HEIGHT):
-    topLeft, topRight, bottomLeft, bottomRight = getPerspectivePoints(highres_scale)
-    perspectiveMatrix = getPerspectiveMatrix(topLeft, topRight, bottomLeft, bottomRight)
+    perspectiveMatrix = getPerspectiveMatrix(highres_scale)
     fgbg = cv2.createBackgroundSubtractorMOG2()
     prefix = '../img/taxi/'
     frame = cv2.imread(prefix + path)
     frame = resizeFrame(frame, 0.5)
     img = resizeFrame(frame, highres_scale)
     laneDetection(img, fgbg, perspectiveMatrix, scaled_height, highres_scale)
-    cv2.waitKey(3000)
+    cv2.waitKey(0)
 
 
 def videoDemo(filename, highres_scale=0.5, scaled_height=Constants.IMG_SCALED_HEIGHT):
@@ -45,8 +48,8 @@ def videoDemo(filename, highres_scale=0.5, scaled_height=Constants.IMG_SCALED_HE
 
 
 if __name__ == '__main__':
-    # pictureDemo('taxi_straight.png')
+    pictureDemo('taxi_straight.png')
     # pictureDemo('taxi_side.png')
-    # pictureDemo('taxi_curve.png')
+    pictureDemo('taxi_curve.png')
     videoDemo('taxi_intersect.mp4') # framerate of 29.97
     # videoDemo('taxi_trim.mp4') # framerate of 29.97
